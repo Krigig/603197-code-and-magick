9c677ad2f8ecf0e182eec796949aa83df1aea4bb
Kristina <vasileva_kri@list.ru>
2018-11-26 09:48:26 +0200

    Стилизация кода

--- js/setup.js
+++ js/setup.js
@@ -1,13 +1,11 @@
+'use strict';
+
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
 var similarListElement = userDialog.querySelector('.setup-similar-list');
 
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
-
-
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var fornames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
@@ -16,11 +14,11 @@ var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var namesX = [];
 
-var random = function(array) {
+var random = function (array) {
   return Math.floor(Math.random() * array.length);
-}
+};
 
-for (var i =0 ; i < 4; i++) {
+for (var i = 0; i < 4; i++) {
   var j = random(names);
   var k = random(fornames);
 
@@ -29,9 +27,9 @@ for (var i =0 ; i < 4; i++) {
 
 var wizards = [];
 
-for (var i =0 ; i < 4; i++) {
-  var j = random(namesX);
-  var k = random(coatColors);
+for (i = 0; i < 4; i++) {
+  j = random(namesX);
+  k = random(coatColors);
   var m = random(eyesColors);
 
   wizards[i] = {};
@@ -48,11 +46,11 @@ var renderWizard = function (wizard) {
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
 
   return wizardElement;
-}
+};
 
 var fragment = document.createDocumentFragment();
 
-for (var i = 0; i < wizards.length; i++) {
+for (i = 0; i < wizards.length; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
 }
 similarListElement.appendChild(fragment);
